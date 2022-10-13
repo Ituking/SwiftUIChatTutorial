e675f6c93fc529b7ef0a512b1c8a03aab53a5f36
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-13 12:59:39 +0900

    Initial Commit

---
+++ SwiftUIChatTutorial/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIChatTutorial
+//
+//  Created by 大久保樹 on 2022/10/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIChatTutorial/SwiftUIChatTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatTutorialApp.swift
+//  SwiftUIChatTutorial
+//
+//  Created by 大久保樹 on 2022/10/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
