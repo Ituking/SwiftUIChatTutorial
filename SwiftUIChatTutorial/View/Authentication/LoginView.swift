3977e438759c5bb645c1265c4ca26610fd34b698
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-13 14:28:00 +0900

    Swift進捗
    SwiftUIChatTutorialの作成

---
+++ SwiftUIChatTutorial/View/Authentication/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  SwiftUIChatTutorial
+//
+//  Created by 大久保樹 on 2022/10/13.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ SwiftUIChatTutorial/View/Authentication/RegistrationView.swift
@@ -0,0 +1,20 @@
+//
+//  RegistrationView.swift
+//  SwiftUIChatTutorial
+//
+//  Created by 大久保樹 on 2022/10/13.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RegistrationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistrationView()
+    }
+}
