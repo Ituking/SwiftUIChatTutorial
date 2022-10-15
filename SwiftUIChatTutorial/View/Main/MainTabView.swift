11570ddccb7f8a6d115562105b10bdf5fc1d600b
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-16 01:58:10 +0900

    Swift進捗
    SwiftUIChatTutorialの作成

---
+++ SwiftUIChatTutorial/View/Main/MainTabView.swift
@@ -0,0 +1,23 @@
+//
+//  MainTabView.swift
+//  SwiftUIChatTutorial
+//
+//  Created by 大久保樹 on 2022/10/16.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        NavigationView {
+            // tab view
+            // each view for tab like channel chats and settings
+        }
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
