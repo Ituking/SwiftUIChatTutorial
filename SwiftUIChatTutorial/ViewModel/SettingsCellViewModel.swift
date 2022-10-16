5e3a0b3b9c9b251bf18c242931c5de2bfbd8200d
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-16 19:47:38 +0900

    Swift進捗
    SwiftUIChatTutorialの作成

---
+++ SwiftUIChatTutorial/ViewModel/SettingsCellViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  SettingsCellViewModel.swift
+//  SwiftUIChatTutorial
+//
+//  Created by 大久保樹 on 2022/10/16.
+//
+
+enum SerringsCellViewModel: Int, CaseIterable {
+    case account
+    case notifications
+    case starredMessages
+    
+    var title: String {
+        switch self {
+        case .account: return "Account"
+        case .notifications: return "Notifications"
+        case .starredMessages: return "Starred Messages"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .account: return "key.fill"
+        case .notifications: return "bell.badge.fill"
+        case .starredMessages: return "star.fill"
+        }
+    }
+    
+    var backgroundColor: String {
+        switch self {
+        case .account: return "Account"
+        case .notifications: return "Notifications"
+        case .starredMessages: return "Starred Messages"
+        }
+    }
+}
