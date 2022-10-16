c42449882058657386376ead18f546ba456de2e2
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-16 13:31:46 +0900

    Swift進捗
    SwiftUIChatTutorialの作成

--- SwiftUIChatTutorial/View/Main/MainTabView.swift
+++ SwiftUIChatTutorial/View/Main/MainTabView.swift
@@ -8,24 +8,51 @@
 import SwiftUI
 
 struct MainTabView: View {
+    @State private var selectedIndex = 0
+    
     var body: some View {
         NavigationView {
-            TabView {
+            TabView(selection: $selectedIndex) {
                 ConversationsView()
+                    .onTapGesture {
+                        print("Selected index in \(selectedIndex)")
+                        selectedIndex = 0
+                    }
                     .tabItem {
                         Image(systemName: "bubble.left")
                     }
+                    .tag(0)
                 
                 ChannelsView()
+                    .onTapGesture {
+                        print("Selected index in \(selectedIndex)")
+                        selectedIndex = 1
+                    }
                     .tabItem {
                         Image(systemName: "bubble.left.and.bubble.right")
                     }
+                    .tag(1)
                 
                 SettingsView()
+                    .onTapGesture {
+                        print("Selected index in \(selectedIndex)")
+                        selectedIndex = 2
+                    }
                     .tabItem {
                         Image(systemName: "gear")
                     }
+                    .tag(2)
             }
+            .navigationTitle(tabTitle)
+        }
+    }
+    
+    var tabTitle: String {
+        switch selectedIndex {
+        case 0: return "Chats"
+        case 1: return "Channels"
+        case 2: return "Settings"
+        default: return ""
         }
     }
 }
