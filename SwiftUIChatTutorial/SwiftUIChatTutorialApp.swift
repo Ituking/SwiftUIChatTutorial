efeb2b64e10db8fee37fe2cbdbca24793f15483d
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-22 11:43:31 +0900

    Swift進捗
    SwiftUIChatTutorialの作成

--- SwiftUIChatTutorial/SwiftUIChatTutorialApp.swift
+++ SwiftUIChatTutorial/SwiftUIChatTutorialApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct SwiftUIChatTutorialApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             MainTabView()
