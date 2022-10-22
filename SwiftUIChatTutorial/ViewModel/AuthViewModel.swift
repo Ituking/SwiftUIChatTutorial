ba5305478d1468db18964c68d9fb0a04f4b2b2fe
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-22 12:15:00 +0900

    Swift進捗
    SwiftUIChatTutorialの作成

---
+++ SwiftUIChatTutorial/ViewModel/AuthViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  AuthViewModel.swift
+//  SwiftUIChatTutorial
+//
+//  Created by 大久保樹 on 2022/10/22.
+//
+
+import Foundation
+
+class AuthViewModel: NSObject, ObservableObject {
+    
+    func login() {
+        
+    }
+    
+    func register() {
+        
+    }
+    
+    func uploadProfileImage() {
+        
+    }
+    
+    func signout() {
+        
+    }
+}
