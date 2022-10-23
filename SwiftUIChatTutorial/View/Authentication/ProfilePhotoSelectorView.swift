8bba6b3d94a001569eaf898e5cdbb8b722f54c84
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-23 23:39:04 +0900

    Swift進捗
    SwiftUIChatTutorialの作成

---
+++ SwiftUIChatTutorial/View/Authentication/ProfilePhotoSelectorView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfilePhotoSelectorView.swift
+//  SwiftUIChatTutorial
+//
+//  Created by 大久保樹 on 2022/10/23.
+//
+
+import SwiftUI
+
+struct ProfilePhotoSelectorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfilePhotoSelectorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfilePhotoSelectorView()
+    }
+}
