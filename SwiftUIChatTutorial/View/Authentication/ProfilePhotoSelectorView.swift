9d311aa3eed8b274d60a82ea0f732629026d6706
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-23 23:43:06 +0900

    Swift進捗
    SwiftUIChatTutorialの作成

--- SwiftUIChatTutorial/View/Authentication/ProfilePhotoSelectorView.swift
+++ SwiftUIChatTutorial/View/Authentication/ProfilePhotoSelectorView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ProfilePhotoSelectorView: View {
+    @State private var imagePickerPresented = false
+    @State private var selectedImage: UIImage?
+    @State private var profileImage: Image?
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
