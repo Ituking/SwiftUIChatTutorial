e74083b12b403eb5b2ef44fa3d31631c02b61ed9
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-25 08:43:16 +0900

    Swift進捗
    SwiftUIChatTutorialの作成

--- SwiftUIChatTutorial/View/Authentication/ProfilePhotoSelectorView.swift
+++ SwiftUIChatTutorial/View/Authentication/ProfilePhotoSelectorView.swift
@@ -35,13 +35,40 @@ struct ProfilePhotoSelectorView: View {
                         .foregroundColor(.black)
                 }
             })
-//            .sheet(isPresented: $imagePickerPresented, onDismiss: loadImage, content: {
-//                ImagePicker(image: $selectedImage)
-//            })
+            .sheet(isPresented: $imagePickerPresented, onDismiss: loadImage, content: {
+                ImagePicker(image: $selectedImage)
+            })
             
             Text(profileImage == nil ? "Select a profile photo" : "Great! Tap below to continue")
                 .font(.system(size: 20, weight: .semibold))
+            
+            if let image = selectedImage {
+                Button(action: {
+                    viewModel.uploadProfileImage()
+                }, label: {
+                    Text("Continue")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(width: 340, height: 50)
+                        .background(Color.blue)
+                        .clipShape(Capsule())
+                        .padding()
+                })
+                .shadow(color: .gray, radius: 10, x: 0.0, y: 0.0)
+                .padding(.top, 24)
+            }
+            
+            Spacer()
+            
         }
+        .navigationBarBackButtonHidden(true)
+    }
+    
+    func loadImage() {
+        guard let selectedImage = selectedImage else {
+            return
+        }
+        profileImage = Image(uiImage: selectedImage)
     }
 }
 
