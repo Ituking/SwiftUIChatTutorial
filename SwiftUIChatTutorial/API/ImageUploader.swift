b653aa835507a7c1115aa57aa8a682de02363f78
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-25 12:42:49 +0900

    Swift進捗
    SwiftUIChatTutorialの作成

--- SwiftUIChatTutorial/API/ImageUploader.swift
+++ SwiftUIChatTutorial/API/ImageUploader.swift
@@ -12,5 +12,8 @@ struct ImageUploader {
     
     static func uploadImage(image: UIImage, completion: @escaping(String) -> Void) {
         guard let imageData = image.jpegData(compressionQuality: 0.5) else { return }
+        
+        let filename = NSUUID().uuidString
+        let ref = Storage.storage().reference(withPath: "/profile_images/\(filename)")
     }
 }
