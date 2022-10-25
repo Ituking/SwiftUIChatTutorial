859e69bddf459158e72cc6e464f95b131dff1e88
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-25 12:38:10 +0900

    Swift進捗
    SwiftUIChatTutorialの作成

---
+++ SwiftUIChatTutorial/API/ImageUploader.swift
@@ -0,0 +1,16 @@
+//
+//  ImageUploader.swift
+//  SwiftUIChatTutorial
+//
+//  Created by 大久保樹 on 2022/10/25.
+//
+
+import Firebase
+import UIKit
+
+struct ImageUploader {
+    
+    static func uploadImage(image: UIImage, completion: @escaping(String) -> Void) {
+        guard let imageData = image.jpegData(compressionQuality: 0.5) else { return }
+    }
+}
