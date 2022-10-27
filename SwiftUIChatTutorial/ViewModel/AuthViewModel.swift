a102f4e3ce6c6e59a9178b322361fa4cd16b3f5a
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-27 23:41:52 +0900

    Swift進捗
    SwiftUIChatTutorialの作成

--- SwiftUIChatTutorial/ViewModel/AuthViewModel.swift
+++ SwiftUIChatTutorial/ViewModel/AuthViewModel.swift
@@ -14,6 +14,7 @@ class AuthViewModel: NSObject, ObservableObject {
     private var tempCurrentUser: FirebaseAuth.User?
     
     override init() {
+        print("DEBUG: Auth view model did init..")
         userSession = Auth.auth().currentUser
     }
     
