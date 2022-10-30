1a4c696ed4d45a40bfc8b16ea1e40d182ce6e6d4
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-31 02:54:55 +0900

    Swift進捗
    SwiftUIChatTutorialの作成

--- SwiftUIChatTutorial/ViewModel/NewMessageViewModel.swift
+++ SwiftUIChatTutorial/ViewModel/NewMessageViewModel.swift
@@ -18,7 +18,7 @@ class NewMessageViewModel: ObservableObject {
     func fetchUsers() {
         COLLECTION_USERS.getDocuments { snapshot, _ in
             guard let documents = snapshot?.documents else { return }
-            self.users = documents.compactMap({ try? $0.data(as: User.self) })
+            self.users = documents.compactMap({ try? $0.data(as: User.self) }).filter({ $0.id != AuthViewModel.shared.userSession?.uid })
         }
     }
 }
