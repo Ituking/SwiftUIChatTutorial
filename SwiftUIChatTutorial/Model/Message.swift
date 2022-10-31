19034c398615896e46b2c062929008461714e097
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-31 13:30:40 +0900

    Swift進捗
    SwiftUIChatTutorialの作成

--- SwiftUIChatTutorial/Model/Message.swift
+++ SwiftUIChatTutorial/Model/Message.swift
@@ -5,10 +5,14 @@
 //  Created by 大久保樹 on 2022/10/22.
 //
 
-import Foundation
+import FirebaseFirestoreSwift
+import Firebase
 
-struct Message: Identifiable {
-    let id = NSUUID().uuidString
-    let isFromCurrentUser: Bool
-    let messageText: String
+struct Message: Identifiable, Decodable {
+    @DocumentID var id: String?
+    let fromId: String
+    let told: String
+    let read: String
+    let text: String
+    let timestamp: Timestamp
 }
