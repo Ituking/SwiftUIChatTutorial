c8b23ddf658f9cf1758bb4331dd15f9cafa79c5d
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-31 13:33:26 +0900

    Swift進捗
    SwiftUIChatTutorialの作成

--- SwiftUIChatTutorial/View/Chats/ChatView.swift
+++ SwiftUIChatTutorial/View/Chats/ChatView.swift
@@ -22,7 +22,7 @@ struct ChatView: View {
             ScrollView {
                 VStack(alignment: .leading, spacing: 12) {
                     ForEach(viewModel.messages) { message in
-                        MessageView(isFromCurrentUser: message.isFromCurrentUser, messageText: message.messageText)
+//                        MessageView(isFromCurrentUser: message.isFromCurrentUser, messageText: message.messageText)
                     }
                 }
             }
--- SwiftUIChatTutorial/ViewModel/ChatViewModel.swift
+++ SwiftUIChatTutorial/ViewModel/ChatViewModel.swift
@@ -12,23 +12,10 @@ class ChatViewModel: ObservableObject {
     @Published var messages = [Message]()
     
     init() {
-        messages = mockMessages
-    }
-    
-    var mockMessages: [Message] {
-        [
-            .init(isFromCurrentUser: true, messageText: "Hey guys! We have a gift for you."),
-            .init(isFromCurrentUser: false, messageText: "No Music, No Life."),
-            .init(isFromCurrentUser: true, messageText: "For once, Don't Do it."),
-            .init(isFromCurrentUser: true, messageText: "I'm lovin' it."),
-            .init(isFromCurrentUser: false, messageText: "Talk to you later!"),
-            .init(isFromCurrentUser: true, messageText: "Just Do It."),
-            .init(isFromCurrentUser: true, messageText: "Think Different.")
-        ]
+        
     }
     
     func sendMessage(_ messageText: String) {
-        let message = Message(isFromCurrentUser: true, messageText: messageText)
-        messages.append(message)
+
     }
 }
