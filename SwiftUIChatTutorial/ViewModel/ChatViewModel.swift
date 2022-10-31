962892ef60f27515c0927cf3aaa2607e96889b78
Ituking <77528963+Ituking@users.noreply.github.com>
2022-10-31 14:00:49 +0900

    Swift進捗
    SwiftUIChatTutorialの作成

--- SwiftUIChatTutorial/ViewModel/ChatViewModel.swift
+++ SwiftUIChatTutorial/ViewModel/ChatViewModel.swift
@@ -5,7 +5,7 @@
 //  Created by 大久保樹 on 2022/10/22.
 //
 
-import Foundation
+import Firebase
 
 class ChatViewModel: ObservableObject {
     @Published var messages = [Message]()
@@ -22,5 +22,17 @@ class ChatViewModel: ObservableObject {
         guard let chatPartnerId = user.id else {
             return
         }
+        
+        let currentUserRef = COLLECTION_MESSAGES.document(currentUid).collection(chatPartnerId).document()
+        let chatPartnerRef = COLLECTION_MESSAGES.document(chatPartnerId).collection(currentUid)
+        
+        let messageId = currentUserRef.documentID
+        
+        let data: [String: Any] = ["text": messageText,
+                                   "fromId": currentUid,
+                                   "toId": chatPartnerId,
+                                   "read": false,
+                                   "timestamp": Timestamp(date: Date())
+        ]
     }
 }
