a14f25820560a451035bb6efe42b0dc98daf3fb9
Ituking <77528963+Ituking@users.noreply.github.com>
2022-11-06 19:17:33 +0900

    Swift進捗
    SwiftUIChatTutorialの作成

--- SwiftUIChatTutorial/SwiftUIChatTutorialApp.swift
+++ SwiftUIChatTutorial/SwiftUIChatTutorialApp.swift
@@ -11,13 +11,22 @@ import Firebase
 @main
 struct SwiftUIChatTutorialApp: App {
     
-    init() {
-        FirebaseApp.configure()
-    }
+//    init() {
+//        FirebaseApp.configure()
+//    }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
             ContentView().environmentObject(AuthViewModel.shared)
+        }
+    }
+    
+    class AppDelegate:NSObject,UIApplicationDelegate{
+        
+        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+            FirebaseApp.configure()
+            return true
         }
     }
 }
